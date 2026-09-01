2ea2a6fd5effc1ad672d05f52b5bdbc14c63fb81
Ayo Adebesin <victorayo234@gmail.com>
2026-09-01 12:01:19 +0100

    fix: stop committing .vercel/output, forcing real builds on Vercel

--- .vercel/output/static/assets/chevron-down-mrnAaZ2T.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./button-uZPvZ7cB.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
--- .vercel/output/static/assets/loader-circle-B3dbG67I.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./button-uZPvZ7cB.js";var t=e(`loader-circle`,[[`path`,{d:`M21 12a9 9 0 1 1-6.219-8.56`,key:`13zald`}]]);export{t};
